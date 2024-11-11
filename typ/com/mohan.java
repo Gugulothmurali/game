e29ed9d7b46ad8bcb838ceeb7ac14eb9cc2d9bb2
Gugulothmurali <gugulothmurali6@gmail>
2024-11-11 16:55:27 +0530

    second brandch push

---
+++ typ/com/mohan.java
@@ -0,0 +1,66 @@
+package typ.com;
+
+public class mohan 
+{
+
+	public static void main(String[] args)
+	{
+		
+		
+		
+ int rum ;
+ rum =525;
+ System.out.println(rum);
+ 
+ 
+ String Name = "my self guguloth mohan";
+ String biodata = "i am from kesamudram mahabubabad dist ";
+ String  institute="i have complete my 10th in ss concept high school in hanamkonda with the aggregate of 8.7CGPA. ";
+ String institute2 = "i complete my inter in TS model college in kalwala with the stream of bipc and with the aggregate of 62%";
+ String inst3 = "i complete my UG in vaagdevi degree and pg college in hanamkonda, with the stream of BSC (BZCS)with the aggregate of 7.23CGPA";
+ System.out.println(Name);
+ System.out.println(biodata);
+ System.out.println(institute);
+ System.out.println(institute2);
+ System.out.println(inst3);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ String movie = "god father";
+String Hero = "chranjeevi";
+String Heroine="nayanthara";
+String Producer = "albert s.ruddy";
+String Choreograph = "prabhu devu";
+System.out.println(movie+Hero+Heroine+Producer+Choreograph);
+ 
+int n = 365 ;
+byte k = (byte)n;
+System.out.println(k);
+
+byte  m = 56;
+int b = (int)m;
+System.out.println(b);
+
+ byte l = 25;
+ short u = (short)l;
+ System.out.println(u);
+ float f = 323.2f;
+ double d = (double)f;
+ System.out.println(d);
+ 
+ char ch =65;
+ System.out.println("ch : "+ch);
+ 
+   int a=5 ,g=6 ,c=7;
+   String val=(a>g)?(a>g?6+"5 is great)":"6 is great"): (g>c?7+"6 is big": "7 is big");
+   System.out.println(val);
+	}
+}
+
+
+
+
